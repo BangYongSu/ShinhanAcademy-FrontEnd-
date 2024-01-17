009f8e9da57ffd52f7a3ab4bdb08240fc5a5f691
DESKTOP-3O6KIK7\User <eunkyung211@naver.com>
2024-01-17 14:27:33 +0900

    module test

---
+++ public/go1.js
@@ -0,0 +1,6 @@
+import { arr, arr2, getName, info } from "./common.js";
+arr2[1] = 88;
+console.log("go1:", info.loc);
+console.log("go1:", arr);
+console.log("go1:", arr2);
+console.log("go1:", getName());
