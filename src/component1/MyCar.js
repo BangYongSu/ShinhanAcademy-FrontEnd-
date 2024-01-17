a7a0769c957e255d1d277d749cfdfb008f14e9f4
DESKTOP-3O6KIK7\User <eunkyung211@naver.com>
2024-01-17 11:56:03 +0900

    reactjs initial commit

---
+++ src/component1/MyCar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function MyCar(props) {
+    return (
+        <div>
+            <h1>내 차는 볼보입니다.</h1>
+            <h2>하얀색</h2>
+        </div>
+    );
+}
+
+export default MyCar;
